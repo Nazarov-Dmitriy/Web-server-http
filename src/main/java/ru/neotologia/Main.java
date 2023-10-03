368eed1a46d94083b746bf20786f0bb6cbf29741
Dmitriy Nazarov <difat1@inbox.ru>
2023-10-04 00:25:30 +0300

    feature/query

--- src/main/java/ru/neotologia/Main.java
+++ src/main/java/ru/neotologia/Main.java
@@ -1,16 +1,55 @@
 package ru.neotologia;
 
+
+import ru.neotologia.hendler.Handler;
+import ru.neotologia.request.Request;
 import ru.neotologia.server.Server;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+
     public static void main(String[] args) {
         var port = Integer.parseInt(readSettings());
-        Server server = new Server(port);
-        server.start();
+        final var server = new Server();
 
+        server.addHandler("GET", "/messages",
+                (request, response) -> {
+                    System.out.println(request.getBody());
+                    final var filePath = Path.of(".", "public", request.getPath() + ".html");
+                    final var mimeType = Files.probeContentType(filePath);
+                    final var length = Files.size(filePath);
+                    response.write(("HTTP/1.1 " + 200 + "\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n").getBytes());
+
+                    Files.copy(filePath, response);
+                    response.flush();
+                });
+
+        server.addHandler("POST", "/messages",
+                (request, response) -> {
+                    final var filePath = Path.of(".", "public", request.getPath() + "Post.html");
+                    final var mimeType = Files.probeContentType(filePath);
+                    final var length = Files.size(filePath);
+                    response.write(("HTTP/1.1 " + 200 + "\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n").getBytes());
+
+                    Files.copy(filePath, response);
+                    response.flush();
+                });
+
+        server.listen(port);
     }
 
     public static String readSettings() {
--- src/main/java/ru/neotologia/server/Server.java
+++ src/main/java/ru/neotologia/server/Server.java
@@ -1,20 +1,21 @@
 package ru.neotologia.server;
 
 import ru.neotologia.clientHandler.ClientHandler;
+import ru.neotologia.hendler.Handler;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server extends Thread {
     static int port;
     static ExecutorService threadPool = Executors.newFixedThreadPool(64);
+    public static final Map<String, Map<String, Handler>> handlerList = new HashMap<>();
 
-    public Server(int port) {
-        Server.port = port;
-        System.out.println("Server started on port " + port);
-    }
+
 
     public void run() {
         try (final var serverSocket = new ServerSocket(port)) {
@@ -30,4 +31,19 @@ public class Server extends Thread {
         }
     }
 
+    public void listen(int port){
+        Server.port = port;
+        System.out.println("Server started on port " + port);
+        start();
+    }
+
+
+    public synchronized void addHandler(String method, String path, Handler handler) {
+        if (!handlerList.containsKey(path)) {
+            handlerList.put(path, new HashMap<>());
+        }
+        if (!handlerList.get(path).containsKey(method)) {
+            handlerList.get(path).put(method, handler);
+        }
+    }
 }
